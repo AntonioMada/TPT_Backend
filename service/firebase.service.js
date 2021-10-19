d329768ecbc9a23dcce95d1d5c846b9968c52c31
AntonioMada <80627474+AntonioMada@users.noreply.github.com>
2021-10-19 22:25:57 +0400

    Update firebase.service.js
    Modify notification

--- service/firebase.service.js
+++ service/firebase.service.js
@@ -6,9 +6,9 @@ const postmsg = async () => {
         "title": "Hi admin", 
         "body": "Un utilisateur a éffectué un pari"
         },
-        "to":"deCSzrWwU6vUMZOtaN0kRk:APA91bGbtpnhhAihTSoOUc-rd_M0uBJUNk7gLJ0vn0eQzsmdYGoodihnMXVFWDQk3STg_iBAeaCQePzuonXvf73AsZcuEt7Pg2G-96DVK1ekbfWupOhlnEh9yIrpdsa8JHXe1qYjM9JA"
+        "to":"fW2f1yafBfq6UQoD1p-Z06:APA91bGLauHt6yZtTkxaHRnmQJ1rGNPDv5MfY4riWOeag0okmJCbTRCi9MOswVzY6biyWtRq3scKFTLgLiWgEGhrQ2LP-AOVyCV4s9EapLJgbdaOAL_SqDZBPur00HNA9LPAhoHk1aAJ"
         }
-    
+    //deCSzrWwU6vUMZOtaN0kRk:APA91bGbtpnhhAihTSoOUc-rd_M0uBJUNk7gLJ0vn0eQzsmdYGoodihnMXVFWDQk3STg_iBAeaCQePzuonXvf73AsZcuEt7Pg2G-96DVK1ekbfWupOhlnEh9yIrpdsa8JHXe1qYjM9JA
     let headers = {
         'Content-Type': 'application/json',
         'Authorization': 'key=AAAA-UOHAMM:APA91bE7xDdzuktya8O4p2YMMxwz8oDWs-iJwdnx8FSDxfQX_oaLBGV2R3R8wfzqktdbOymOTKZ9YISYsNziEqnO6NG4FbXH7CjhYh_kfh_z17BYQAMmImRtmeS5_KWD8R3MPuPOAkkD' }
